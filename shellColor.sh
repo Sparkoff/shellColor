757e02ab88c80d7e04a76f0371243fffa355c193
Jorin Perquy <jperquy@appnexus.com>
2016-08-31 14:11:27 +0200

    comment script

--- shellColor.sh
+++ shellColor.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+## bash script with color in terminal
+# echo -e \033[style;colorm : with style
+# echo -e \033[colorm       : without style
+
+## Styles (optional)
+#  0 Normal Characters
+#  1 Bold Characters
+#  4 Underlined Characters
+#  5 Blinking Characters
+
 ## Colors [Foreground 3, Background 4]
 #  0 black
 #  1 red
@@ -10,31 +20,45 @@
 #  6 cyan
 #  7 white
 #  9 reset
+
+
 colors=( "blk" "red" "grn" "yel" "blu" "mag" "cyn" "wht" )
 
-## Styles
-#  0 Normal Characters
-#  1 Bold Characters
-#  4 Underlined Characters
-#  5 Blinking Characters
-
-
-for (( group = -1; group < 8; group++ )); do
-    echo -e "    --------------------------------------------------------------"
-    if [ $group -eq -1 ]; then
+## Print table
+# Each row will have two lines : Normal and Bold style, and will represent a background colors
+# Each column is a foreground color
+for (( row = -1; row < 8; row++ )); do
+    # Start line
+    if [ $row -eq -1 ]; then
+        # Start default line (line -1)
+        # Initialize style Bolod (1) on second line
         line1="DFT :  ";
-        line2="    :  ";
+        line2="    : \033[1m ";
     else
-        name=${colors[group]}
-        line1="$(echo $name | tr 'a-z' 'A-Z') : \033[4${group}m ";
-        line2="    : \033[4${group}m ";
+        # Start colored line (line 0-7)
+        # Search color name
+        name=${colors[row]}
+
+        # Initialize lines start and foreground (4* color)
+        # Initialize style Bolod (1) on second line
+        line1="$(echo $name | tr 'a-z' 'A-Z') : \033[4${row}m ";
+        line2="    : \033[1;4${row}m ";
     fi
+
+    # Fill lines's columns
     for (( color = 0; color < 8; color++ )); do
-        line1+="  \033[3${color}m${colors[color]}  ";
-        line2+="  \033[1;3${color}m${colors[color]}  ";
+        # Compose column with color name with right color
+        column="  \033[3${color}m${colors[color]}  "
+        line1+=$column;
+        line2+=$column;
     done
-    line1+=" \033[39;49m :";
-    line2+=" \033[0;39;49m :";
+
+    # Reset settings (style bold (1), foreground (3*) and background (4*))
+    line1+=" \033[39;49m :"; # reset only colors
+    line2+=" \033[0;39;49m :"; # reset colors and style
+
+    # Print separator and line
+    echo -e "    --------------------------------------------------------------"
     echo -e "$line1"
     echo -e "$line2"
 done
